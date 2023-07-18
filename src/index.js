7a79f0aaa0a2c015d70456fed6aeb639614f65df
MykhailoVasyliuk <misha.vasyliuk@gmail.com>
2023-07-18 22:11:03 +0300

    add simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import {fetchImages} from './js/fetchImages'
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const inputRef = document.querySelector('input[name="searchQuery"]');
 const cardRef  = document.querySelector('.gallery');
@@ -20,23 +22,36 @@ function onQueryMarcup(queryObject){
 
      return queryObject.hits.map(({ webformatURL , largeImageURL , tags , likes , views , comments , downloads }) => 
        
-        `<div class="photo-card">
-            <img src="${webformatURL}" alt="" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                <b>${likes}</b>
+        `<a href="${largeImageURL}">
+            <div class="photo-card">
+                <img src="${webformatURL}" alt="" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">${likes}
+                    <b>Likes</b>
+                    </p>
+                    <p class="info-item">
+                    <b>${views}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>${comments}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>${downloads}</b>
                 </p>
-                <p class="info-item">
-                <b>${views}</b>
-                </p>
-                <p class="info-item">
-                <b>${comments}</b>
-                </p>
-                <p class="info-item">
-                <b>${downloads}</b>
-            </p>
+                </div>
             </div>
-        </div>` 
+        </a>` 
        ).join("");
 
 }
+
+let galleryImg = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionPosition: 'bottom',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+ console.log(galleryImg)
